cd04cd8b3c6fe70be0fd20fd82d2e808222d25d3
Kashi639 <sawantsohan555@gmail.com>
2025-06-13 00:23:57 +0530

    home page ui complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
 import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<UserLayout />}>
-        { /* User Layout */}
-      </Route>
-      <Route>{ /* Admin Layout */}</Route>
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route>{/* Admin Layout */}</Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
