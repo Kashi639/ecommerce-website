6ab16cf8e9617b51fc155652ab2929808fbe32ba
Kashi639 <sawantsohan555@gmail.com>
2025-06-27 22:34:44 +0530

    admin sidebar and homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import Checkout from "./components/Cart/Checkout";
 import OrderConfirmationPage from "./pages/OrderConfirmationPage";
 import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrdersPage from "./pages/MyOrdersPage";
+import AdminLayout from "./components/Admin/AdminLayout";
+import AdminHomePage from "./pages/AdminHomePage";
 
 const App = () => {
   return (
@@ -25,11 +27,16 @@ const App = () => {
           <Route path="collection/:collection" element={<CollectionPage />} />
           <Route path="product/:id" element={<ProductDetails />} />
           <Route path="checkout" element={<Checkout />} />
-          <Route path="order-confirmation" element={<OrderConfirmationPage />} />
+          <Route
+            path="order-confirmation"
+            element={<OrderConfirmationPage />}
+          />
           <Route path="order/:id" element={<OrderDetailsPage />} />
           <Route path="my-orders" element={<MyOrdersPage />} />
         </Route>
-        <Route>{/* Admin Layout */}</Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
